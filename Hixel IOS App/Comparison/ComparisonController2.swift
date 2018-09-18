32860543a64f43e2229278c0df6dc8d829fbe990
Naveen Gaur <naveengaur800@gmail.com>
2018-09-19 02:14:46 +1000

    Bar Graph has been added. Right now the Comparison view needs a default of 3 companies but it will be configured for 2 later on. Although Bar Graph has a hardcoded value but can be changed using the auto layout constraints which makes it dynamic.

--- Hixel IOS App/Comparison/ComparisonController2.swift
+++ Hixel IOS App/Comparison/ComparisonController2.swift
@@ -10,21 +10,119 @@ import UIKit
 
 class ComparisonController2: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
     //var selectedCompanies = ["Apple Inc","Samsung Corp","Alphabet","Bmw","Facebook"]
+    
+    // Arrays that holds the Financial Indicators values
+    var financialIndicatorsValue = [4,3,2]
+    
+    // All Horizontal line
+    @IBOutlet weak var H_Bar3: UIView!
+    @IBOutlet weak var H_Bar4: UIView!
+    @IBOutlet weak var H_Bar2: UIView!
+    
+    
+    
+    @IBOutlet weak var HBar3: UILabel!
 
+    // All Bars
+    @IBOutlet weak var bar1: UIView!
+    @IBOutlet weak var bar2: UIView!
+    @IBOutlet weak var bar3: UIView!
+    
+    // MARK: All Graph labels
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    
+    
+    @IBOutlet weak var graphContainerView: UIView!
+    
     @IBOutlet weak var graphHeaderView: UIView!
     @IBOutlet weak var collectionViewB: UICollectionView!
     // Contains all the companies user have selected
     var Aselected: [TempCompany] = []
     let shapeLayer = CAShapeLayer()
     
-
+    @IBOutlet weak var horizontalLine: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         graphHeaderView.dropShadow()
+        graphContainerView.dropShadow()
+        graphContainerView.layer.borderColor = UIColor.black.cgColor
+        graphContainerView.layer.borderWidth = 0.4
         
+        setGraphLabels()
+       bar1.backgroundColor =  cherryPickColour(value: financialIndicatorsValue[0])
+       bar2.backgroundColor =  cherryPickColour(value: financialIndicatorsValue[1])
+       bar3.backgroundColor =  cherryPickColour(value: financialIndicatorsValue[2])
 
+        // intital constraints on the bar graphs
+        // Auto Layout for Bar 1
+        bar1.translatesAutoresizingMaskIntoConstraints = false
+        bar1.topAnchor.constraint(equalTo: H_Bar4.bottomAnchor, constant: 0).isActive = true
+        bar1.bottomAnchor.constraint(equalTo: horizontalLine.topAnchor, constant: 0).isActive = true
+        bar1.leftAnchor.constraint(equalTo: graphContainerView.rightAnchor, constant: 63).isActive = true
+        bar1.rightAnchor.constraint(equalTo: graphContainerView.leftAnchor, constant: 95).isActive = true
+        bar1.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        bar1.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        // Auto Layout for Bar 2
+        bar2.isHidden = false
+        bar2.translatesAutoresizingMaskIntoConstraints = false
+        bar2.topAnchor.constraint(equalTo: H_Bar3.bottomAnchor, constant: 0).isActive = true
+        bar2.bottomAnchor.constraint(equalTo: horizontalLine.topAnchor, constant: 0).isActive = true
+        bar2.leftAnchor.constraint(equalTo: graphContainerView.rightAnchor, constant: 63).isActive = true
+        bar2.rightAnchor.constraint(equalTo: graphContainerView.leftAnchor, constant: 200).isActive = true
+        bar2.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        bar2.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        // Auto Layout for Bar 3
+        bar3.isHidden = false
+        bar3.translatesAutoresizingMaskIntoConstraints = false
+        bar3.topAnchor.constraint(equalTo: H_Bar2.bottomAnchor, constant: 0).isActive = true
+        bar3.bottomAnchor.constraint(equalTo: horizontalLine.topAnchor, constant: 0).isActive = true
+        bar3.leftAnchor.constraint(equalTo: graphContainerView.rightAnchor, constant: 63).isActive = true
+        bar3.rightAnchor.constraint(equalTo: graphContainerView.leftAnchor, constant: 305).isActive = true
+        bar3.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        bar3.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    func setGraphLabels()
+    {
+        label1.text = Aselected[0].name
+        label2.text = Aselected[1].name
+        label3.text = Aselected[2].name
+
+    }
+    
+    func cherryPickColour(value: Int) -> UIColor
+    {
+        if (value > 3)
+        {
+            return UIColor.init(netHex: 0x60CBB2)
+        }
+        
+        else if (value == 3)
+        {
+            return UIColor.init(netHex: 0xFADD8A)
+            
+        }
+            
+        else{
+            return UIColor.init(netHex: 0xED6985)
+
+            
+        }
+    }
+    
     // Dismiss the Comparison view when the back button is pressed.
     @IBAction func back_button(_ sender: Any) {
         self.dismiss(animated: true)
