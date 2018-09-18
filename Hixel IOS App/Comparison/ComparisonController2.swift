c026e5247cf2ea6819d32e630d01f35aecc3ee75
Naveen Gaur <naveengaur800@gmail.com>
2018-09-18 20:39:50 +1000

    Second Collection View Added on Comparison.

---
+++ Hixel IOS App/Comparison/ComparisonController2.swift
@@ -0,0 +1,53 @@
+//
+//  ComparisonController2.swift
+//  Hixel IOS App
+//
+//  Created by Naveen Gaur on 18/9/18.
+//  Copyright © 2018 Naveen Gaur. All rights reserved.
+//
+
+import UIKit
+
+class ComparisonController2: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    //var selectedCompanies = ["Apple Inc","Samsung Corp","Alphabet","Bmw","Facebook"]
+
+    @IBOutlet weak var collectionViewB: UICollectionView!
+    // Contains all the companies user have selected
+    var Aselected: [TempCompany] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    // Dismiss the Comparison view when the back button is pressed.
+    @IBAction func back_button(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Aselected.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Inside this if statement setup the score chart
+        if collectionView == self.collectionViewB {
+            let cellB = collectionView.dequeueReusableCell(withReuseIdentifier: "score", for: indexPath) as! ScoreCollectionViewCell
+            
+            return cellB
+        }
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ComparisonCell", for: indexPath) as! ComparisonCollectionViewCell
+        
+        cell.company_name.text = Aselected[indexPath.row].name
+        cell.layer.cornerRadius = 10.0
+        // The color of the cell should depend upon the
+        //cell.backgroundColor = UIColor.init(netHex: 0x)
+       // print(Aselected.count)
+        return cell
+    
+    }
+ 
+
+}
---
+++ Hixel IOS App/Comparison/ScoreCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ScoreCollectionViewCell.swift
+//  Hixel IOS App
+//
+//  Created by Naveen Gaur on 18/9/18.
+//  Copyright © 2018 Naveen Gaur. All rights reserved.
+//
+
+import UIKit
+
+class ScoreCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var companyName: UILabel!
+}
